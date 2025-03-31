7eca66e1465f8214b10f32629eb7c592a4b18644
ManWithNothingToLose <ahmedbaranalmas@gmail.com>
2025-03-31 22:51:27 +0300

    some errors fixed

--- app/src/main/java/com/example/week11solution/MainActivity.java
+++ app/src/main/java/com/example/week11solution/MainActivity.java
@@ -20,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText searchBar;
     private ViewPagerAdapter viewPagerAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
         searchBar = findViewById(R.id.searchBar);
